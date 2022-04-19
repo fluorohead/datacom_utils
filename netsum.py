064eb2bb97416a27d99bbe66b4e2529ef378cd0b
gurovi <i.a.gurov@mgts.ru>
2022-04-19 23:13:21 +0300

    second commit

--- netsum.py
+++ netsum.py
@@ -35,6 +35,7 @@ def OctetsAndMaskToDWORD_v4(strOneLine):
 
 def WordsAndMaskToLong_v6(strOneLine):
     strPermittedSymbols = '/0123456789:ABCDEFabcdef'
+
     flagWolfBillet = False
     for intK in range(0, len(strOneLine)):
         if not (strOneLine[intK] in strPermittedSymbols):
