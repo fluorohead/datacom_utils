5dce23a4034e8809902b4641672a5206f9d6f5a6
ivan <fluorohead@gmail.com>
2022-04-20 22:29:05 +0300

    another commit

--- recur.py
+++ recur.py
@@ -1,2 +1,18 @@
+from stat import S_ISDIR
 import os
+
+def GetAllDirsIn(strTop):
+    print(f'dir > {strTop}')
+    for strNext in os.listdir(strTop):
+        strNextSearchIn = f'{strTop}{os.sep}{strNext}'
+        if S_ISDIR(os.stat(strNextSearchIn, follow_symlinks = True).st_mode):
+            GetAllDirsIn(strNextSearchIn)
+        else:
+            print(f'file > {strNextSearchIn}')
+    pass
+
+GetAllDirsIn(os.getcwd())
+
+
+
 
