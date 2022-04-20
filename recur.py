ed009257c64d350d36987257b7fa6c0fd14a3185
ivan <fluorohead@gmail.com>
2022-04-20 22:29:05 +0300

    another commit

--- recur.py
+++ recur.py
@@ -1,17 +1,17 @@
 from stat import S_ISDIR
 import os
 
-def GetAllDirsIn(strTop):
+def RecursiveFileSearch(strTop):
     print(f'dir > {strTop}')
     for strNext in os.listdir(strTop):
         strNextSearchIn = f'{strTop}{os.sep}{strNext}'
         if S_ISDIR(os.stat(strNextSearchIn, follow_symlinks = True).st_mode):
-            GetAllDirsIn(strNextSearchIn)
+            RecursiveFileSearch(strNextSearchIn)
         else:
             print(f'file > {strNextSearchIn}')
     pass
 
-GetAllDirsIn(os.getcwd())
+RecursiveFileSearch(os.getcwd())
 
 
 
