95a9f512dd274d3ffdeb05991d752a021c793658
ivan <fluorohead@gmail.com>
2022-04-22 21:44:42 +0300

    some refactoring done

--- chk_ospf.py
+++ chk_ospf.py
@@ -14,10 +14,7 @@ def WorkWithFile(strFN):
     else:
         #print('Opening Ok')
         list_OSPFInterfaces = []
-        list_LDPInterfaces = []
-        list_MCASTInterfaces = []
-        list_PIMInterfaces = []
-        for strLine in fileIn: # searching for router ospf 1 beginning
+        for strLine in fileIn: # searching for 'router ospf' beginning
             if re.match('^router ospf \d{1,5}$\n', strLine):
                 #print('router ospf pattern found')
                 break
@@ -30,58 +27,26 @@ def WorkWithFile(strFN):
                 break
         if list_OSPFInterfaces:
             #print(f'OSPF : {list_OSPFInterfaces}')
-            flagLDP_Present = False
-            flagMCAST_Present = False
-            flagPIM_Present = False
-            for strLine in fileIn: #searching for mpls ldp beginning
-                if re.match('^mpls ldp$\n', strLine):
-                    #print('mpls ldp pattern found')
-                    flagLDP_Present = True
-                    break
-            if flagLDP_Present:
-                for strLine in fileIn: # searching for mpls ldp ending
-                    if strLine != strPartEnding:
-    #                    print(strLine, end='')
-                        if re.match('^ interface.*', strLine):
-                            list_LDPInterfaces.append(strLine.strip().strip(strStrip))
-                    else:
+            def FindPartition(strRegexp, strInterface, strExclude):
+                flagPresent = False
+                listInterfaces = []
+                for strLine2 in fileIn:
+                    if re.match(strRegexp, strLine2):
+                        flagPresent = True
                         break
-                #print(f'LDP : {list_LDPInterfaces}')
-            else:
-                #print('No MPLS LDP partition found')
-                fileIn.seek(0)
-            for strLine in fileIn: #searching for multicast-routing beginning
-                if re.match('^multicast-routing.*', strLine):
-                    #print('multicast-routing partition found')
-                    flagMCAST_Present = True
-                    break
-            if flagMCAST_Present:
-                for strLine in fileIn: #searching for multicast-routing ending
-                    if strLine != strPartEnding:
-                        if re.match('^ {2}interface.*', strLine):
-                            list_MCASTInterfaces.append(strLine.strip().strip(strStrip))
-                    else:
-                        break
-                #print(f'MCAST : {list_MCASTInterfaces}')
-            else:
-                #print('No multicast-routing partition found')
-                fileIn.seek(0)
-            for strLine in fileIn: #searching for router pim beginning
-                if re.match('^router pim.*', strLine):
-                    #print('router pim partition found')
-                    flagPIM_Present = True
-                    break
-            if flagPIM_Present:
-                for strLine in fileIn: #searching for router pim ending
-                    if strLine != strPartEnding:
-                        if re.match('^ {2}interface.*', strLine):
-                            list_PIMInterfaces.append(strLine.strip().strip(strStrip))
-                    else:
-                        break
-                #print(f'PIM : {list_PIMInterfaces}')
-            else:
-                #print('No router pim partition found')
-                fileIn.seek(0)
+                if flagPresent:
+                    for strLine2 in fileIn:
+                        if strLine2 != strPartEnding:
+                            if re.match(strInterface, strLine2):
+                                listInterfaces.append(strLine2.strip().strip(strStrip))
+                        else:
+                            break
+                else:
+                    fileIn.seek(0)
+                return listInterfaces
+            list_LDPInterfaces = FindPartition('^mpls ldp$', '^ interface.*')
+            list_MCASTInterfaces = FindPartition('^multicast-routing$\n', '^ {2}interface.*')
+            list_PIMInterfaces = FindPartition('^router pim$\n', '^ {2}interface.*')
             # main searching
             for strZ in list_OSPFInterfaces:
                 if strZ not in list_LDPInterfaces:
